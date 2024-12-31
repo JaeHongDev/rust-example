77ff78f144e90d907653434d37e360ed1e8dd09c
JaehongDev <println@kakao.com>
2024-12-31 17:12:45 +0900

    test: 문자열 -> 3자리 숫자 검증 테스트 코드 작성

--- baseball/src/lib.rs
+++ baseball/src/lib.rs
@@ -1,6 +1,24 @@
+use std::ops::Not;
+
 pub fn add(left: usize, right: usize) -> usize {
     left + right
 }
+
+
+pub fn str_to_baseball_num(input: &str) -> Vec<u32> {
+    let vec: Vec<u32> = str::chars(input).map(|c| c.to_digit(10))
+        .map(|option_c| option_c.expect("문자열이 포함됐습니다"))
+        .filter(|n| (1..=9).contains(n))
+        .collect();
+
+    vec.len()
+        .eq(&3)
+        .not()
+        .then(|| {panic!("3자리가 아닙니다")});
+
+    vec
+}
+
 
 #[cfg(test)]
 mod tests {
@@ -25,4 +43,11 @@ mod tests {
         println!("{:?}", vec);
     }
 
+    #[test]
+    #[should_panic(expected = "3자리가 아닙니다")]
+    fn 문자열은_3자리이며_1에서_9사이의_숫자임(){
+        let input = "2124";
+
+        str_to_baseball_num(input);
+    }
 }
