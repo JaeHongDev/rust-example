07b5efb5bb4c0d17d4586dab4d1a67bc7305bdec
JaehongDev <println@kakao.com>
2025-01-02 16:36:22 +0900

    feat: input view 기능 구현

---
+++ baseball/src/view/input_view.rs
@@ -0,0 +1,34 @@
+use std::io::{stdin, stdout, Write};
+
+pub trait Input {
+    fn read_number(option: Option<&str>) -> i64;
+    fn read_line(option: Option<&str>) -> String;
+}
+
+pub struct ConsoleInputView;
+
+impl Input for ConsoleInputView {
+    fn read_number(option: Option<&str>) -> i64 {
+        print(option);
+        read_line().parse().expect("숫자 이외의 값이 입력됐습니다.")
+    }
+
+    fn read_line(option: Option<&str>) -> String {
+        print(option);
+        read_line()
+    }
+}
+
+fn read_line() -> String {
+    stdin().lines().next().unwrap().unwrap()
+}
+
+fn print(option: Option<&str>) {
+    match option {
+        None => {}
+        Some(msg) => {
+            print!("{}", msg);
+            stdout().flush().unwrap();
+        }
+    }
+}
