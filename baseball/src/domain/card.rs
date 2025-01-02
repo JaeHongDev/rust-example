ab9d96f3e91c52cffcdaf399e9bc2bf67fc8f8e7
JaehongDev <println@kakao.com>
2025-01-02 15:15:46 +0900

    feat: 카드 생성 기능 구현

--- baseball/src/domain/card.rs
+++ baseball/src/domain/card.rs
@@ -1,3 +1,5 @@
+use std::ops::Not;
+
 pub struct Card {
     pos: i8,
     value: i8,
@@ -5,6 +7,14 @@ pub struct Card {
 
 impl Card {
     pub fn new(pos: i8, value: i8) -> Self {
+        if pos < 0 {
+            panic!("card의 pos는 0 밑의 값을 허용하지 않습니다.");
+        }
+
+        if (1..=9).contains(&value).not() {
+            panic!("card의 값은 1~9사이의 숫자로 구성해야 합니다.")
+        }
+
         Self { pos, value }
     }
 }
--- baseball/tests/card.rs
+++ baseball/tests/card.rs
@@ -3,7 +3,18 @@ mod card_test {
     use baseball::domain::card::Card;
 
     #[test]
-    fn 카드_생성_테스트() {
-        Card::new(1, 2);
+    #[should_panic]
+    fn 카드의_pos는_0에서_3사이의_숫자_이하로_떨어질_수_없습니다() {
+        (-2..=1).for_each(|x| {
+            Card::new(x, 1);
+        })
+    }
+
+    #[test]
+    #[should_panic]
+    fn 카드의_value는_1에서_9사이의_숫자로_구성해야합니다() {
+        (-10..=0).chain(10..=15).for_each(|x| {
+            Card::new(1, x);
+        })
     }
 }
