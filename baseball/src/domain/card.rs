06c506121889e3d2d0e953c75fcc90e9eacaa6ee
JaehongDev <println@kakao.com>
2025-01-02 15:30:23 +0900

    refactor: card 검증 로직 분리하기

--- baseball/src/domain/card.rs
+++ baseball/src/domain/card.rs
@@ -7,14 +7,22 @@ pub struct Card {
 
 impl Card {
     pub fn new(pos: i8, value: i8) -> Self {
-        if pos < 0 {
-            panic!("card의 pos는 0 밑의 값을 허용하지 않습니다.");
-        }
-
-        if (1..=9).contains(&value).not() {
-            panic!("card의 값은 1~9사이의 숫자로 구성해야 합니다.")
-        }
+        Self::validate_card_pos(pos);
+        Self::validate_card_value(value);
 
         Self { pos, value }
+    }
+
+    fn validate_card_pos(pos: i8) {
+        pos.eq(&0)
+            .then(|| panic!("card의 pos는 0 밑의 값을 허용하지 않습니다."));
+    }
+
+    fn validate_card_value(value: i8) {
+        value
+            .ge(&1)
+            .le(&9)
+            .not()
+            .then(|| panic!("card의 값은 1~9사이의 숫자로 구성해야 합니다."));
     }
 }
