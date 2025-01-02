eabd7bf56eb3776ffeb4b7e79776da2b14dea448
JaehongDev <println@kakao.com>
2025-01-02 15:27:20 +0900

    refactor: 검증 기능 분리하기

--- baseball/src/domain/player.rs
+++ baseball/src/domain/player.rs
@@ -9,12 +9,15 @@ pub struct Player {
 
 impl Player {
     pub fn new(cards: Vec<Card>) -> Self {
+        Self::validate_card_length(&cards);
+        Self { cards }
+    }
+
+    fn validate_card_length(cards: &Vec<Card>) {
         cards
             .len()
             .eq(&3)
             .not()
             .then(|| panic!("사용자는 3자리 숫자를 가져야 합니다."));
-
-        Self { cards }
     }
 }
