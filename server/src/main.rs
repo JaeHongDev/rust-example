3a1f9cb3a2e57ed65406297ef64c218176e99f27
JaehongDev <println@kakao.com>
2025-01-05 18:22:36 +0900

    feat: 간단한 html 파일을 내려받도록 변경

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,16 +1,45 @@
-use std::io::Write;
+use std::io::{BufRead, BufReader, Write};
 use std::net::{TcpListener, TcpStream};
+use std::thread;
+
+
+const contents: &str = r#"
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>Hello!</title>
+  </head>
+  <body>
+    <h1>Hello!</h1>
+    <p>Hi from Rust</p>
+  </body>
+</html>
+"#;
+
 
 fn handle_client(mut stream: TcpStream) {
 
-    let response = "HTTP/1.1 200 OK\r\n\
-                    Content-Type: text/plain\r\n\
-                    \r\n\
-                    Hello, World!\r\n";
+    let buf_reader = BufReader::new(&mut stream);
 
-    stream.write(response.as_bytes()).expect("Failed to write response");
-    stream.flush().expect("Failed to flush stream");
-    println!("{:?}", stream);
+    let http_request: Vec<_>= buf_reader.lines()
+        .map(|result| result.unwrap())
+        .take_while(|line| !line.is_empty())
+        .collect();
+
+    println!("{:#?}", http_request);
+
+    let response = "HTTP/1.1 200 OK\r\n\r\n";
+
+
+
+    let status_line = "HTTP/1.1 200 OK";
+    let length = contents.len();
+
+    let response =
+        format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+
+    stream.write_all(response.as_bytes()).unwrap();
 }
 
 fn main() {
@@ -19,7 +48,10 @@ fn main() {
 
     for stream in listener.unwrap().incoming(){
 
-        stream.and_then(|stream| Ok(handle_client(stream))).expect("TODO: panic message");
+        stream.and_then(|stream| Ok(thread::spawn(|| {
+            handle_client(stream)
+        })))
+            .expect("TODO: panic message");
     }
 
 }
